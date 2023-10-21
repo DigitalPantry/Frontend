b6f4d1cce352d30ea5b54993337f0fd37f8fad0c
JacksonRhea <jacksonrhea12@gmail.com>
2023-10-20 22:16:03 -0700

    Search Bar + Filter Modal

--- app/(app)/(pages)/pantry.tsx
+++ app/(app)/(pages)/pantry.tsx
@@ -1,17 +1,19 @@
 import styles from "../../../global/styles";
 import colors from "../../../global/colors";
-import { Pressable, ScrollView, TextInput, Image, Text, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { Pressable, ScrollView, TextInput, Image, Text, TouchableWithoutFeedback, Keyboard, View } from "react-native";
 import { Stack, useRouter } from 'expo-router';
 import { useState } from "react";
+import SearchBarFilter from "../../../components/searchBar";
 
 const pantry: React.FC = () => {
     const router = useRouter();
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <ScrollView style={styles.background}>
+            <View style={styles.background}>
+                <SearchBarFilter></SearchBarFilter>
                 <Text>pantry page</Text>
-            </ScrollView>
+            </View>
         </TouchableWithoutFeedback>
     )
 }
